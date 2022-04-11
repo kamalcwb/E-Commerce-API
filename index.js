5b5a7c2fe79b2734d722e18017cc7ad8bd44f752
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-04-11 15:48:15 -0300

    Registro
    Adicionado sistema de registro de contas

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("Conectado ao Database")).catch((err) => console.log(err));
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
