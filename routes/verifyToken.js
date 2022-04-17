9cbbfdb36af4a1516c68a8e126d91ec5769bedc1
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-04-17 16:19:28 -0300

    Verificação de usuário
    Verificação de usuários e privilégios

---
+++ routes/verifyToken.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken")
+
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+            if (err) {
+                return res.status(403).json("Token inválido")
+                req.user = user;
+            }
+        })
+    } else {
+        return res.status(401).json("Falha de autenticação")
+    }
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json("Erro de hierarquia");
+        }
+    });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json("Erro de hierarquia");
+        }
+    });
+};
+
+module.exports = { verifyToken }
